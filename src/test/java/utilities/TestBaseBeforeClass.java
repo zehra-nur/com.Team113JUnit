ecc75ed86cd9aadd2abdca87831c0491a1b80d86
zehra-nur <zehranuravcibasi@gmail.com>
2023-06-24 10:50:25 +0300

    TestBaseBeforeClass

--- src/test/java/utilities/TestBaseBeforeClass.java
+++ src/test/java/utilities/TestBaseBeforeClass.java
@@ -2,7 +2,9 @@ package utilities;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -12,15 +14,15 @@ public class TestBaseBeforeClass {
 
     protected static WebDriver driver;
 
-    @Before
-    public void setup(){
+    @BeforeClass
+    public static void setup(){
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
     }
-    @After
-    public void teardown(){
+    @AfterClass
+    public static void teardown(){
         driver.quit();
     }
 }
